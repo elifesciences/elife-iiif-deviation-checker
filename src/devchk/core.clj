be1752882ac89300f9766dfc97c6465e30ace123
Luke <lsh-0@users.noreply.github.com>
2020-07-23 18:28:02 +0930

    polish

--- src/devchk/core.clj
+++ src/devchk/core.clj
@@ -12,7 +12,7 @@
   (json/parse-string string true))
 
 (defn read-report
-  "if a report exists, create an index of results so we don't have to process them again"
+  "if a report exists, create an index of uri->md5 results so we don't have to process them again"
   []
   (let [report-file (java.io.File. "report.json")]
     (when (.exists report-file)
@@ -24,37 +24,34 @@
 
 (def report-idx (read-report))
 
-(def cache-root "")
-(def cache-dir (-> cache-root (str "cache") java.io.File. .getAbsolutePath str))
-(def image-cache-dir (-> cache-root (str "image-cache") java.io.File. .getAbsolutePath str))
-(def clear-image-cache true) ;; set to false to preserve image cache (good for debugging)
-
 ;; number of parallel image processors to run, one per-core
 (def num-processors (-> (Runtime/getRuntime) .availableProcessors))
 
-;; hit the production iiif server for image requests. (good for debugging)
-(def use-prod-iiif true)
+(def use-prod-iiif true) ;; use the `iiif--prod` server
+(def clear-image-cache true) ;; set to false to preserve image cache (good for debugging)
+
+(def cache-root "") ;; "/ext/devchk/", when you need a different cache dir (ensure trailing slash)
+(def cache-dir (-> cache-root (str "cache") java.io.File. .getAbsolutePath str)) ;; "/path/to/cache"
+(def image-cache-dir (-> cache-root (str "image-cache") java.io.File. .getAbsolutePath str)) ;; "/path/to/cache/image-cache"
 
 ;; create our cache dirs, if they don't already exist
 (-> cache-dir java.io.File. .mkdirs)
 (-> image-cache-dir java.io.File. .mkdirs)
 
 (def image-chan
-  "this is where we'll put the links to images we find while walking the API that need to be processed.
-  accepts N images before blocking"
+  "this is where we'll put the image data we find that needs processing."
   (async/chan 10))
 
 (def results-chan
-  "this is where we'll put the results of processing the image.
-  accepts N results before blocking"
+  "this is where we'll put the results of processing images."
   (async/chan 10))
 
 (def stats
   "this is where we accumulate some ongoing stats about the state of the program as it's running."
-  (atom {:num-images 0
-         :num-processed 0
-         :num-deviations 0
-         :num-errors 0}))
+  (atom {:num-images 0     ;; number of images found
+         :num-processed 0  ;; number of images processed
+         :num-deviations 0 ;; number of deviations we've found
+         :num-errors 0}))  ;; number of errors encountered processing images
 
 ;; utils
 
@@ -74,36 +71,35 @@
 
 (let [lock (Object.)]
   (defn log
-    "synchronised printing. ensures everybody gets a line on stdout to themselves"
+    "synchronised file writing. ensures everybody gets a line to themselves"
     [level & args]
     (locking lock
       (let [filename (-> level name (str ".json"))
             extra (rest args)
             msg (json/generate-string (if (not (empty? extra))
-                                                 {:message (first args) :extra extra}
-                                                 {:message (first args)}))]
+                                        {:message (first args) :extra extra}
+                                        {:message (first args)}))]
         (spit filename (str msg "\n") :append true)))))
 
 (defn fs-safe-cache-key
   "safely encode a URI to something that can live cached on the filesystem"
   [url]
   (let [enc (java.util.Base64/getUrlEncoder)]
-    (->> url
-      str .getBytes (.encodeToString enc) str)))
+    (->> url str .getBytes (.encodeToString enc) str)))
 
 (defn spy
+  "print the value of `x` and then return without modifying."
   [x]
-  (stderr "(spying)" x)
-  x)
+  (stderr "(spying)" x) x)
 
 (defn sh
-  "calls `shell/sh` with given `args`, printing a friendly version of the command to stdout before doing so."
+  "calls `shell/sh` with given `args` and records a friendly version of the command in the debug log."
   [& args]
   (log :debug (str "$ " (clojure.string/join " " args)))
   (apply shell/sh args))
 
 (defn GET
-  "fetch url, write to file, returns text"
+  "fetch url, write to cache, returns textual body"
   [url]
   (let [output-file (str cache-dir "/" (fs-safe-cache-key url))]
     (if (.exists (java.io.File. output-file))
@@ -115,7 +111,7 @@
           (log :error err))))))
 
 (defn GET-binary
-  "fetch url, write output to given output-file. returns output file on success"
+  "fetch url, write bytes to given `output-file`, returns `output-file` on success or `nil` on error."
   [url output-file]
   (if (and (.exists (java.io.File. output-file))
            (> (.length (java.io.File. output-file)) 0))
@@ -126,9 +122,8 @@
         output-file
         (log :error err)))))
 
-
 (defn url?
-  "predicate, returns true if given value can be parsed as a URL"
+  "predicate, returns `true` if given value can be parsed as a URL"
   [x]
   (try
     (java.net.URL. x)
@@ -137,7 +132,7 @@
       false)))
 
 (defn msid?
-  "predicate, returns true if given value can be parsed as a manuscript ID"
+  "predicate, returns `true` if given value can be parsed as an elife manuscript ID (msid)"
   [x]
   (try
     (-> x java.lang.Integer/valueOf pos-int?)
@@ -145,17 +140,17 @@
       false)))
 
 (defn delete-file
+  "deletes a file if it exists and is a file"
   [path]
   (when-let [obj (-> path java.io.File.)]
     (when (.isFile obj)
       (.delete obj))))
 
-;; api handling
+;; image discovery and extraction
 
-
-(defn -find-images
-  "visits everything in `data` and looks for images.
-  when an image is found it is added to the `image-chan`."
+(defn find-images
+  "visits everything in given `data` struct (article-json or a sub-structure of it), looking for images.
+  when an image is found it is added to `image-chan` for processing."
   [data]
   (let [pred (fn [val]
                (and (map? val)
@@ -181,34 +176,28 @@
       (extractor data)
       ;; we didn't find any images at this level, descend deeper
       (cond
-        ;; if we're a map, visit each key+val and test each val
+        ;; if we're a map, visit each key+val pair, testing each `val`
         (map? data) (run! (fn [[key val]]
-                            [key (-find-images val)]) data)
-        ;; if we're a list of data, test each one
-        (sequential? data) (run! -find-images data)
-
+                            [key (find-images val)]) data)
+        ;; if we're a list of data, test each item in list
+        (sequential? data) (run! find-images data)
         ;; scalar value probably, just exit
         :else nil))))
 
-(defn find-images
-  [data]
-  (-find-images data))
-
 (defn get-article
+  "given the manuscript ID `msid`, downloads the latest version of an elife article"
   [msid]
-  (let [api "https://api.elifesciences.org"
-        article-url (str api "/articles/" msid)
-        keys-as-keywords true]
-    (-> article-url GET (json/parse-string keys-as-keywords))))
+  (let [article-url (str "https://api.elifesciences.org/articles/" msid)]
+    (-> article-url GET json-parse-string)))
 
 (defn articles
+  "downloads a report from the Observer project and extracts the manuscript IDs of *all* published articles"
   []
   (let [url "https://observer.elifesciences.org/report/published-article-index.csv"
         csv-data (doall (csv/read-csv (GET url)))
-
-        ;; these are lazy fetches and won't actually happen until we hit `find-images` below
-        article-id-list (->> csv-data rest (map first))
-        article-list (map get-article (take 15 article-id-list))]
+        article-list (->> csv-data rest (map first)
+                          (take 20) ;; download N articles. disable this to download *all* articles
+                          (map get-article))]
 
     ;; images are embedded throughout the article-json. we need to visit every value and look for these:
     ;; {"assets" [
@@ -223,23 +212,22 @@
     ;;                         "filename" "elife-56613-fig2-v2.jpg"}},
     ;;      "label" "Figure 2",
     ;;      "title" "Growth of Mtb strain H37Rv during exposure to ofloxacin.",
-    ;;      "type" "image"}
-    ;; ]}
+    ;;      "type" "image"}]}
 
     (run! find-images article-list))
   nil)
 
 (defn find-all
-  "finds sources of iiif data and extracts them. Results are put on the `image-chan`."
+  "finds sources of iiif data and extracts them. Results are put on the `image-chan` for processing."
   []
   (async/go
     (articles)
-    ;; digests?
-    ;; no more data to scrape, signal the chan to close
-    (async/close! image-chan)
-    ))
+    ;; (digests)
+    ;; no more data to scrape, signal the channel to close
+    (async/close! image-chan)))
 
 (defn find-single-image
+  "adds a single iiif image to the `image-chan` for processing."
   [iiif-url]
   (async/put! image-chan {:label "unknown label"
                           :title "unknown title"
@@ -249,46 +237,41 @@
   (increment :num-images))
 
 (defn find-single-article
+  "adds all of the images in a single elife article to the `image-chan` for processing."
   [msid]
-  (log :debug "got msid" msid)
   (find-images (get-article msid)))
 
-
-;; processing
-
+;; image processing
 
 (defn compare-images
   "given two images, calls ImageMagick with the right incantation.
-  Call to ImageMagick is wrapped in a 'timeout' of a 20 seconds with a --kill after 40 seconds.
-  If you're running this script without a swap disk, and somehow manage to max out your ram and
-  the kernel oom killer hasn't run yet, 'timeout' will catch you."
+  the call to ImageMagick is wrapped in a 'timeout' of a 20 seconds with a --kill after 40 seconds.
+  ImageMagick can die and take an underpowered machine with it if we don't stop it."
   [image-path-1 image-path-2 & [attempt]]
-  (let [output-path (str (java.io.File/createTempFile "devchk" ".jpg"))
+  (let [comparison-file (str (java.io.File/createTempFile "devchk" ".jpg"))
 
         {:keys [exit err out]}
         (sh "timeout" "--kill-after=40s" "20s"
             ;;"magick" "compare" "-metric" "pae" ;; IM 7 only. ubuntu 18.04 still on IM 6
             "compare" "-metric" "pae"
             "-quiet"
-            image-path-1 image-path-2 output-path)
+            image-path-1 image-path-2 comparison-file)
 
         ;; 'ae' is Absolute Error - the number of pixels that are different between the two
         ;; we're not really interested in this value
         ;; 'pae' is Peak Absolute Error, a value between 0 and 1 of the maximum difference between two pixels
-        ;; 0 is perfectly identical
-        ;; 1 is completely different
-        ;; setting the fuzz factor to this value as a percentage will get you a successful result
+        ;; 0 is perfectly identical and 1 is completely different
+        ;; setting the fuzz factor to the `pae` value as a percentage will get you a successful result
         [ae pae] (rest (re-matches #"(\d+) \((\d\.\d+)\)" err))
-
         pae (some-> pae java.lang.Float/valueOf)]
     
     (cond
-      ;; first failure, try again
+      ;; ImageMagick exited with a non-zero result. this is the first attempt, try again.
       (and (> exit 1)
            (= attempt nil)) (compare-images image-path-1 image-path-2 2)
 
-      ;; image magick died. probably because iiif died and returned text.
-      ;; this isn't the first attempt either. log an error
+      ;; ImageMagick exited with a non-zero result.
+      ;; this is not the first attempt so log the error and give up.
       (> exit 1) (do
                    (increment :num-errors)
                    (log :error "imagemagick return an exit code greater than 1"
@@ -298,43 +281,37 @@
                          :image-1 image-path-1
                          :image-2 image-path-2})))
 
-      {:pae pae
-       :cache {:local-comparison-file output-path
-               :local-original-file image-path-1
-               :local-iiif-file image-path-2}}))
+    {:pae pae
+     :cache {:local-comparison-file comparison-file
+             :local-original-file image-path-1
+             :local-iiif-file image-path-2}}))
 
 (defn image-cache-path
   "images must be stored on the disk long enough to compare them with the iiif derived image.
   there is no guaranteee there won't be a naming conflict so preserve the path structure."
   [image-url]
-  (let [;; /digests/55692/digest-55692.tif
-        path (-> image-url java.net.URL. .getPath java.net.URLDecoder/decode)
-        ;; digest-55692.tif
-        file (-> path java.io.File. .getName)
-        ;; /digests/55692/
-        parent (-> path java.io.File. .getParent)
-        ;; ./image-cache/digests/55692/
-        cache-dir-obj (java.io.File. (str image-cache-dir parent))]
-    ;; create the directory if it doesn't exist
+  (let [path (-> image-url java.net.URL. .getPath java.net.URLDecoder/decode) ;; /digests/55692/digest-55692.tif
+        file (-> path java.io.File. .getName) ;; digest-55692.tif
+        parent (-> path java.io.File. .getParent) ;; /digests/55692/
+        cache-dir-obj (java.io.File. (str image-cache-dir parent))] ;; ./image-cache/digests/55692/
     (when-not (.exists cache-dir-obj)
       (-> cache-dir-obj .mkdirs))
-    (str cache-dir-obj "/" file)))
+    (str cache-dir-obj "/" file))) ;; ./image-cache/digests/55692/digest-55692.tif`
 
 (defn download-image
   "downloads a binary image, returns path to file within the `image-cache`"
   [image-url]
-  (let [output-file (image-cache-path image-url)]
-    (GET-binary image-url output-file)))
+  (GET-binary image-url (image-cache-path image-url)))
 
 (defn iiif-to-s3-url
   "converts a iiif URL to an S3 bucket URL where the original image is stored."
   [iiif-url]
   ;; transformation looks like:
-  ;; https://iiif.elifesciences.org/digests/55692%2Fdigest-55692.tif/full/full/0/default.webp
-  ;; /digests/55692%2Fdigest-55692.tif/full/full/0/default.webp
-  ;; /digests/55692/digest-55692.tif/full/full/0/default.webp
-  ;; /digests/55692/digest-55692.tif
-  ;; https://prod-elife-published.s3.amazonaws.com/digests/55692/digest-55692.tif
+  ;;   https://iiif.elifesciences.org/digests/55692%2Fdigest-55692.tif/full/full/0/default.webp
+  ;;   /digests/55692%2Fdigest-55692.tif/full/full/0/default.webp
+  ;;   /digests/55692/digest-55692.tif/full/full/0/default.webp
+  ;;   /digests/55692/digest-55692.tif
+  ;;   https://prod-elife-published.s3.amazonaws.com/digests/55692/digest-55692.tif
   (let [strip-from-full (fn [path]
                           (last (re-find #"(.+)?/full/full/.*" path)))
         lax-to-articles (fn [path]
@@ -359,7 +336,7 @@
      :md5 (first (split (:out (sh "md5sum" "--binary" image-path)) #" "))}))
 
 (defn -process-image
-  "given an image, generates urls, downloads the images, compares them, tacks on extra image data and returns a map of results"
+  "given an `image`, generates urls, downloads the images, compares them, tacks on extra image data and returns a map of results"
   [image]
   (let [iiif-url (-> image :source :uri)]
     (if (contains? report-idx iiif-url)
@@ -375,10 +352,7 @@
           (not original-image) (log :error "problem downloading original image:" {:original-image original-image})
 
           :else
-          (let [;;iiif-meta (image-meta iiif-image)
-                ;;iiif-meta (assoc iiif-meta :local-uri local-iiif-url)
-                iiif-meta (-> iiif-image image-meta (assoc :local-uri local-iiif-url))
-
+          (let [iiif-meta (-> iiif-image image-meta (assoc :local-uri local-iiif-url))
                 original-meta (image-meta original-image)
 
                 article-id (->> iiif-url (re-find #"elife\-(\d{5})\-") last java.lang.Integer/valueOf)
@@ -391,12 +365,11 @@
             result))))))
 
 (defn process-image
-  "guard function"
   [image]
   (try
     (-process-image image)
     (catch Exception uncaught-exc
-      (log :error "uncaught exception processing image" {:image image})
+      (log :error "uncaught exception processing image" {:image image :exc (str uncaught-exc)})
       (increment :num-errors))))
 
 (defn image-processor
@@ -417,17 +390,14 @@
       (stderr (format "worker %s exiting" my-id))))
   nil)
 
-
 ;; reporting
 
-
 (defn report
-  "this takes results off of `results-chan` and write a report to stdout periodically.
+  "this takes results off of `results-chan` and writes a report to stdout as they are processed.
   we do this on the main thread so it blocks us from exiting until we have no more results left."
   []
   (loop []
     (when-let [result (async/<!! results-chan)]
-
       ;; check for deviations
       (if (or (= (:pae result) 1)
               (= (:pae result) 1.0))
@@ -439,21 +409,15 @@
           (some-> result :cache :local-iiif-file delete-file)
           (some-> result :cache :local-original-file delete-file)))
 
-      ;; ...
-
       (let [stats @stats
             num-remaining (- (:num-images stats)
                              (:num-processed stats))]
-
-        ;; (controls the output somewhat but isn't guaranteed)
         (stderr (select-keys stats [:num-images :num-processed :num-errors :num-deviations]))
         (log :report result)
-
         (when-not (= num-remaining 0)
-          ;; jobs outstanding, pull next job channel
           (recur))))))
 
-;;
+;; bootstrap
 
 (defn parse-args
   [arg-list]
@@ -466,22 +430,15 @@
 (defn -main
   [& args]
   (let [[iiif-url msid] (parse-args args)]
-
     (cond
-      ;; add image to image-chan
       (some? iiif-url) (find-single-image iiif-url)
-
-      ;; fetch article, extract image data
       (some? msid) (find-single-article msid)
-
-      ;; no args given, find all data
       :else (find-all))
 
-    ;; start processing images added by find-data
+    ;; start processing images added by the find-* functions
     (run! image-processor (range num-processors))
 
     ;; polls the results channel and prints progress
-    ;; will exit if no new results after `report-timeout` seconds
     (report)
 
     ;; write the final set of stats to the report
