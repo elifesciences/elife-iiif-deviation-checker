9508a6aca77b2d3e0b9db61f2c70d6fafad13996
Luke <lsh-0@users.noreply.github.com>
2020-07-27 10:32:04 +0930

    removes service restart from disable caching script. too specific

--- disable-loris-caching.sh
+++ disable-loris-caching.sh
@@ -2,4 +2,3 @@
 # tweaks the loris configuration and reloads iiif service
 set -e
 sed --in-place 's/enable_caching = True/enable_caching = False/g' /opt/loris/loris2.conf
-sudo systemctl restart iiif-service
